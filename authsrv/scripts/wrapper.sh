091a9b54ad39eb2d2573c7c11e34de66a2485f31
Gaulois <effet.papillon+gitlab@protonmail.com>
2020-06-26 14:09:39 +0200

    ADD send metadata in sharing folder

--- authsrv/scripts/wrapper.sh
+++ authsrv/scripts/wrapper.sh
@@ -10,9 +10,12 @@ if [ $status -ne 0 ]; then
 fi
 
 cp -f /opt/shibboleth-idp/metadata/idp-metadata.xml /opt/shibboleth-idp/shared-metadata/idp-metadata.xml
+cp -f /opt/shibboleth-idp/metadata/idp-metadata.xml /opt/shibboleth-idp/export_metadata/idp-metadata.xml
 
 # cron activate-metadata
 while sleep 30; do
+  cp -f /opt/shibboleth-idp/export_metadata/*.xml /opt/shibboleth-idp/metadata_sp/.;
+  
   /opt/shibboleth-idp/bin/activate-metadata-docker.sh
   status=$?
   if [ $status -ne 0 ]; then
